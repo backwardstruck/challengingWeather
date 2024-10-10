c21efa3d4ee4b6ef50dd9dc219fd199f479671c9
Rooney <backwardstruck@users.noreply.github.com>
2024-10-10 14:29:23 -0400

    Request locaiton

--- app/src/main/java/com/example/whatstheweather/MainActivity.kt
+++ app/src/main/java/com/example/whatstheweather/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.whatstheweather
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
 import com.example.whatstheweather.ui.theme.WhatsTheWeatherTheme
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -50,6 +56,14 @@ class MainActivity : ComponentActivity() {
                     }
                 }
 
+                RequestLocationPermission { granted ->
+                    if (granted) {
+                        getCurrentLocationAndFetchWeather()
+                    } else if (!lastCity.isNullOrEmpty()) {
+                        weatherViewModel.fetchCoordinates(lastCity)
+                    }
+                }
+
 
                 SearchScreen(
                     viewModel = weatherViewModel,
@@ -63,4 +77,48 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun RequestLocationPermission(onPermissionResult: (Boolean) -> Unit) {
+        val permissionLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            onPermissionResult(isGranted)
+        }
+
+        LaunchedEffect(Unit) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    onPermissionResult(true)
+                }
+                else -> {
+                    permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        }
+    }
+
+    private fun getCurrentLocationAndFetchWeather() {
+        try {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        val lat = location.latitude
+                        val lon = location.longitude
+
+
+                        weatherViewModel.fetchWeather(lat, lon)
+                    } else {
+                        // TODO: Handle case where location is null
+                    }
+                }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+
+        }
+    }
 }
+
