5345a6687dd7f0daff6f24e2cbc70cb499f507f2
Rooney <backwardstruck@users.noreply.github.com>
2024-10-10 15:36:24 -0400

    Cleanup

--- app/src/main/java/com/example/whatstheweather/WeatherViewModel.kt
+++ app/src/main/java/com/example/whatstheweather/WeatherViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.whatstheweather
 
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
