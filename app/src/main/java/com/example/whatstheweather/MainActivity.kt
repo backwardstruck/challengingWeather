506bd533f5fe189f966ccdb3781d11639b05c6ce
Rooney <backwardstruck@users.noreply.github.com>
2024-10-10 10:27:18 -0400

    Update code

--- app/src/main/java/com/example/whatstheweather/MainActivity.kt
+++ app/src/main/java/com/example/whatstheweather/MainActivity.kt
@@ -3,45 +3,36 @@ package com.example.whatstheweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.whatstheweather.ui.theme.WhatsTheWeatherTheme
+import androidx.compose.ui.unit.dp
+import com.example.whatstheweather.WeatherViewModel
 
 class MainActivity : ComponentActivity() {
+    private val weatherViewModel: WeatherViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            WhatsTheWeatherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            WeatherScreen(weatherViewModel)
         }
+
+        weatherViewModel.fetchWeather(44.00,10.00)
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi $name",
-        modifier = modifier
-    )
-}
+fun WeatherScreen(weatherViewModel: WeatherViewModel) {
+    val weatherData = weatherViewModel.weatherData.observeAsState()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WhatsTheWeatherTheme {
-        Greeting("Android")
+    Column(modifier = Modifier.padding(16.dp)) {
+        weatherData.value?.let {
+            Text(text = "Temperature: ${it.main.temp}")
+            Text(text = "Description: ${it.weather[0].description}")
+        }
     }
 }
