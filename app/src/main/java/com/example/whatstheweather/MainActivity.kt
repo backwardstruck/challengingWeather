255f7015e37d3887786cd2dabdf4d34a33159d6f
Rooney <backwardstruck@users.noreply.github.com>
2024-10-10 10:51:42 -0400

    Use screen

--- app/src/main/java/com/example/whatstheweather/MainActivity.kt
+++ app/src/main/java/com/example/whatstheweather/MainActivity.kt
@@ -4,35 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.whatstheweather.WeatherViewModel
+import com.example.whatstheweather.ui.theme.WhatsTheWeatherTheme
 
 class MainActivity : ComponentActivity() {
+
     private val weatherViewModel: WeatherViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherScreen(weatherViewModel)
-        }
 
-        weatherViewModel.fetchWeather(44.00,10.00)
-    }
-}
+            WhatsTheWeatherTheme {
 
-@Composable
-fun WeatherScreen(weatherViewModel: WeatherViewModel) {
-    val weatherData = weatherViewModel.weatherData.observeAsState()
-
-    Column(modifier = Modifier.padding(16.dp)) {
-        weatherData.value?.let {
-            Text(text = "Temperature: ${it.main.temp}")
-            Text(text = "Description: ${it.weather[0].description}")
+                SearchScreen(viewModel = weatherViewModel)
+            }
         }
     }
 }
